9b895fddca1555bb1873bbbebb43ee5086dc65f7
Simon Dell <simon@expressionworks.co.uk>
2017-02-28 11:28:58 +0000

    Showing housing data

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,24 @@
+var data = require('../data.json');
 var express = require('express');
+
 var router = express.Router();
 
+function pagetitle ( subtitle ) {
+  return 'Zoopla - PAGE'.replace( /PAGE/, subtitle );
+}
+
 /* GET home page. */
-router.get('/results', function(req, res, next) {
-  res.render('results', { title: 'Express' });
-});
+router.get('/results?:q',
+  function showResults(req, res, next) {
+    var viewModel = req.query.q === data.area ? data : {};
+
+    viewModel.title = pagetitle( 'Results' );
+    res.render('results', viewModel );
+  }
+);
 
 router.get('/search', function(req, res, next) {
-  res.render('search', { title: 'Express' });
+  res.render('search', { title: pagetitle( 'Search' ) });
 });
 
 module.exports = router;
